0e640cfeb49776a1381adc12f6279860ad4182a1
eidanfili <anning3232@gmail.com>
2019-02-22 10:49:14 -0700

    sudo come

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import Create from './pages/createAcc';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍😍</h1>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/create" component={Create} />
+          </Switch>
+        </Router>
       </div>
     );
   }
