064cf3de1f251be18009edd8515e061826d34929
makennawold <makennawold@gmail.com>
2019-02-22 14:37:56 -0700

    hi

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import kraken from './../images/kraken.png';
+import krakenIcon from './../images/krakenIcon.png'
 
 export default class Home extends Component{
     render() {
@@ -8,24 +9,18 @@ export default class Home extends Component{
             <div className="header">
                 <div className="leftSection">
                     <div className="icon"><img src={kraken}/></div>
-                    <div className="krackenTitle"><h1>Kraken</h1></div>            
-                </div>
-                <div className="dropdownSection">
-                
-                </div>
-                <div className="rightSection">
-                    <div className="profileImage"></div>
-                    <div className="profileBtn">
-                        <p>username</p>
-                    </div>
-                </div>
-            
-        </div>
-        <div className="pageArea">
-        
+                    <div className="krakenTitle">
+                    <h1>Kraken</h1>
+                    </div>            
+                </div>            
+            </div>
+            <div className="pageArea">
+                <p>descriptiom=hn]90zegruyaets</p>
+                <div className="iconLarge"><img src={krakenIcon}/></div>
+                <p>enter kraken</p>
+            </div>
         </div>
         
-        </div>
         )
     }
 }
